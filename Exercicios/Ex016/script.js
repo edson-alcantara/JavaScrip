7b7a7e2f696a5bf50c1b4c0b7257c026949e90b9
Edson Alcantara <alcantaa09@gmail.com>
2026-07-01 20:30:49 -0300

    Contador

--- Exercicios/Ex016/script.js
+++ Exercicios/Ex016/script.js
@@ -12,7 +12,7 @@ function contar(){
         let i = Number(ini.value);
         let f = Number(fim.value);
         let p = Number(passo.value);
-        if(p == 0){
+        if(p <= 0){
             window.alert('Passo Inválido! Considerando 1');
             p = 1;
         }
